f71996e9a113269d8cdadd89c8c7fdeb5700f70c
EamonThomas <93980415+EamonThomas@users.noreply.github.com>
2021-11-17 00:11:38 +0530

    Project 1.2 .py

---
+++ Project 1.2 .py
@@ -0,0 +1,27 @@
+print('FIRST TASK')
+print()
+print()
+
+N1 = input('Input the radius of the circle:')
+area = 3.14*(int(N1))**2
+print()
+
+print('The area of the circle with radius',N1 ,'is: ',area )
+
+
+print() 
+print()
+
+print('SECOND TASK')
+print()
+print()
+
+
+A1 = input('Input the filename :')
+
+if A1[-2:] == 'py':
+   print('The extension of the file is \'python\'')
+elif A1[-2:] == 'cs':
+   print('The extension of the file is \'C#\'')
+else:
+   print('The extension of the file is\'Java\'')
