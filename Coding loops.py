204f41458e0370c430d5a95306e87c25594f6b69
EamonThomas <93980415+EamonThomas@users.noreply.github.com>
2021-11-23 22:13:44 +0530

    Coding loops.py
    This is the code

---
+++ Coding loops.py
@@ -0,0 +1,14 @@
+# Python program to print positive Numbers in a List using for loop
+  
+# list1
+list1 = [12, -7, 5, 64, -14]
+for i in list1:
+    if i >= 0:
+       print(i, end = ",")
+print()
+# list2
+list2 = [12, 14, -95, 3]
+for item in list2:
+    if item >= 0:
+       print(item, end = ",")
+       
